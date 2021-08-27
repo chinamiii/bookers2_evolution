33151840219378598e28ba169a5fc1244ac4cd97
chinamiii <chinami.2929@icloud.com>
2021-08-27 13:43:14 +0000

    [8/27]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'da30f7f2b3a425e987732e534413f6737a9f40566a8d3c163994f2005c5d834cf93510d1f5acbc7af36cc90751abe17ed3e74fc9c8c6c5e538b90b09c0fef9e4'
