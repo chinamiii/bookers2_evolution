96e17c79e83ccfa0102f2fcd02df976308723cc2
chinamiii <chinami.2929@icloud.com>
2021-08-29 06:49:02 +0000

    [follow]

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,18 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about'=> 'homes#about'
+  
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
 
-  resources :books
+  end
 
-  resources :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 
 
 end
